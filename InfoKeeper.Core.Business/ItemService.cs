1645e196c96736be90ef961fe124b132687c97d7
Lily <shirley.fox_@hotmail.com>
2023-03-13 21:03:44 +0100

    Implement basic business logic

---
+++ InfoKeeper.Core.Business/ItemService.cs
@@ -0,0 +1,45 @@
+﻿using InfoKeeper.Core.Business.Abstract;
+using InfoKeeper.Core.Models;
+using InfoKeeper.Infrastructure.Database.Abstract;
+
+namespace InfoKeeper.Core.Business;
+
+public class ItemService : IItemService
+{
+    private readonly IItemDatabase _database;
+
+    public ItemService(IItemDatabase database)
+    {
+        _database = database;
+    }
+
+    public async Task<List<Item>> GetAsync()
+    {
+        return await _database.GetAsync();
+    }
+
+    public async Task<Item?> GetAsync(string id)
+    {
+        return await _database.GetAsync(id);
+    }
+
+    public async Task<Item> CreateAsync(Item item)
+    {
+        return await _database.CreateAsync(item);
+    }
+
+    public async Task<Item?> UpdateAsync(string id, Item item)
+    {
+        return await _database.UpdateAsync(id, item);
+    }
+
+    public async Task<Item?> DeleteAsync(string id)
+    {
+        return await _database.DeleteAsync(id);
+    }
+
+    public async Task<List<Item>> Search(string query)
+    {
+        return await _database.Search(query);
+    }
+}
---
+++ InfoKeeper.Core.Business/TagService.cs
@@ -0,0 +1,40 @@
+﻿using InfoKeeper.Core.Business.Abstract;
+using InfoKeeper.Core.Models;
+using InfoKeeper.Infrastructure.Database.Abstract;
+
+namespace InfoKeeper.Core.Business;
+
+public class TagService : ITagService
+{
+    private readonly ITagDatabase _database;
+
+    public TagService(ITagDatabase database)
+    {
+        _database = database;
+    }
+    
+    public async Task<List<Tag>> GetAsync()
+    {
+        return await _database.GetAsync();
+    }
+
+    public async Task<Tag?> GetAsync(string id)
+    {
+        return await _database.GetAsync(id);
+    }
+
+    public async Task<Tag> CreateAsync(Tag tag)
+    {
+        return await _database.CreateAsync(tag);
+    }
+
+    public async Task<Tag?> UpdateAsync(string id, Tag tag)
+    {
+        return await _database.UpdateAsync(id, tag);
+    }
+
+    public async Task<Tag?> DeleteAsync(string id)
+    {
+        return await _database.DeleteAsync(id);
+    }
+}
