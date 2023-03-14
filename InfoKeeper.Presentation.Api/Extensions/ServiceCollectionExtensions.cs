dccc367f17fa0dadb2b4708359f0bfaffbab3279
Lily <shirley.fox_@hotmail.com>
2023-03-14 17:54:15 +0100

    Move initialization of IServiceCollection to separate class in extension methods

---
+++ InfoKeeper.Presentation.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using InfoKeeper.Configuration.Settings;
+using InfoKeeper.Core.Business;
+using InfoKeeper.Core.Business.Abstract;
+using InfoKeeper.Infrastructure.Database.Abstract;
+using InfoKeeper.Infrastructure.Database.MongoDB;
+using InfoKeeper.Presentation.Api.GraphQL.Mutations;
+using InfoKeeper.Presentation.Api.GraphQL.Queries;
+
+namespace InfoKeeper.Presentation.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAllServices(this IServiceCollection services, 
+        InfoKeeperDatabaseSettings databaseSettings)
+    {
+        services
+            .AddConfiguration(databaseSettings)
+            .AddDatabase()
+            .AddBusinessLogic()
+            .AddGraphQl();
+
+        return services;
+    }
+
+    private static IServiceCollection AddConfiguration(this IServiceCollection services, 
+        InfoKeeperDatabaseSettings databaseSettings)
+    {
+        return services.AddSingleton(databaseSettings);
+    }
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services)
+    {
+        return services.AddTransient<ITagDatabase, TagDatabase>()
+            .AddTransient<IItemDatabase, ItemDatabase>();
+    }
+
+    private static IServiceCollection AddBusinessLogic(this IServiceCollection services)
+    {
+        return services.AddTransient<ITagService, TagService>()
+            .AddTransient<IItemService, ItemService>();
+    }
+
+    private static void AddGraphQl(this IServiceCollection services)
+    {
+        services.AddGraphQLServer()
+            .AddQueryType()
+            .AddTypeExtension<TagQuery>()
+            .AddTypeExtension<ItemQuery>()
+            .AddMutationType()
+            .AddTypeExtension<TagMutation>()
+            .AddTypeExtension<ItemMutation>();
+    }
+}
--- InfoKeeper.Presentation.Api/Program.cs
+++ InfoKeeper.Presentation.Api/Program.cs
@@ -1,31 +1,12 @@
 using InfoKeeper.Configuration.Settings;
-using InfoKeeper.Core.Business;
-using InfoKeeper.Core.Business.Abstract;
-using InfoKeeper.Infrastructure.Database.Abstract;
-using InfoKeeper.Infrastructure.Database.MongoDB;
-using InfoKeeper.Presentation.Api.GraphQL.Mutations;
-using InfoKeeper.Presentation.Api.GraphQL.Queries;
+using InfoKeeper.Presentation.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var databaseSettings = new InfoKeeperDatabaseSettings();
 builder.Configuration.Bind("InfoKeeperDatabaseSettings", databaseSettings);
-builder.Services.AddSingleton(databaseSettings);
 
-builder.Services.AddTransient<ITagDatabase, TagDatabase>();
-builder.Services.AddTransient<IItemDatabase, ItemDatabase>();
-
-builder.Services.AddTransient<ITagService, TagService>();
-builder.Services.AddTransient<IItemService, ItemService>();
-
-builder.Services
-    .AddGraphQLServer()
-    .AddQueryType()
-    .AddTypeExtension<TagQuery>()
-    .AddTypeExtension<ItemQuery>()
-    .AddMutationType()
-    .AddTypeExtension<TagMutation>()
-    .AddTypeExtension<ItemMutation>();
+builder.Services.AddAllServices(databaseSettings);
 
 var app = builder.Build();
 
