b733ecfa9938207ef46a194740a9c603074a14e2
Lily <shirley.fox_@hotmail.com>
2023-03-30 14:39:55 +0200

    Create validators for the models in the business logic layer

---
+++ InfoKeeper.Core.Business/Validators/ItemValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using InfoKeeper.Core.Models;
+
+namespace InfoKeeper.Core.Business.Validators;
+
+public class ItemValidator : AbstractValidator<Item>
+{
+    public ItemValidator()
+    {
+        RuleFor(x => x.Title).NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Content).NotEmpty()
+            .MaximumLength(10_000);
+    }
+}
---
+++ InfoKeeper.Core.Business/Validators/TagValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using InfoKeeper.Core.Models;
+
+namespace InfoKeeper.Core.Business.Validators;
+
+internal class TagValidator : AbstractValidator<Tag>
+{
+    public TagValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.Color).NotEmpty()
+            .Length(6)
+            .Must(x => Int32.TryParse(x, out _));
+    }
+}
